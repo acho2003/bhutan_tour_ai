840f9f40d1f411af70530f12d7f4ba40899dd818
acho2003 <136061373+acho2003@users.noreply.github.com>
2025-10-01 02:36:57 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,15 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0',                     // Make the server accessible externally
+    port: process.env.PORT || 5173,      // Use Render's assigned port
+    allowedHosts: ['bhutan-tour-ai.onrender.com'] // Allow requests from your Render domain
+  }
 })
